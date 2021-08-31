b5538a41cea0883140cd336aa643a0dcbacd49ac
Sami Maxhuni <samimaxhuni@kutia.net>
2021-08-31 20:31:06 +0200

    Welcome page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+import express from 'express';
 import dotenv from 'dotenv-safe';
+import path from 'path';
 
 dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
+const viewPath = process.env.VIEW_PATH || 'views';
+
+app.set('views', path.join(__dirname, viewPath));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, resp) => {
+  resp.render('index', { title: null });
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://${host}:${port}`);
+});
